bf269e5db4accb81d7aac3f4f0be7b59ac4167a2
Ondřej Tichý <ondrejtichy07@gmail.com>
2018-07-17 19:20:44 +0200

    Innitial commit

--- Question21/ViewController.swift
+++ Question21/ViewController.swift
@@ -8,18 +8,93 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController
+{
+    var questions = ["How are you?",
+                     "How old are you?",
+                     "What's your mother's name?",
+                     "Do you have a simbling?",
+                     "Do you like these questions?",
+                     "Do you vote?"]
+   
+    
+    var questionIndex = 0
+    var player = 0
+    
+    @IBOutlet weak var questionsLabel: UILabel!
+    @IBOutlet weak var nextQbutton: UIButton!
+    @IBOutlet weak var nextQbutton2: UIButton!
+    
+    @IBAction func nextButton(_ sender: Any)
+    {
+        checkPlayer()
+    }
+    
+    func checkPlayer()
+    {
+        if player % 2 == 0
+        {
+            nextQbutton2.isHidden = false
+            nextQbutton.isHidden = true
+            nextQbutton2.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+            questionsLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi)
+            
+            testEnd()
+            
+            player += 1
+        }
+        else
+        {
+            nextQbutton.isHidden = false
+            nextQbutton2.isHidden = true
+            questionsLabel.transform = CGAffineTransform(rotationAngle: CGFloat.pi*2)
+            
+            testEnd()
+            
+            player += 1
+        }
+        
+    }
+    
+    func testEnd ()
+    {
+        questionIndex += 1
+        
+            
+        if questionIndex == questions.count
+        {
+            questionsLabel.text = nil
+            nextQbutton2.isHidden = true
+            nextQbutton.isHidden = true
+            
+            let alert = UIAlertController(title: "Notice", message: "That's all! Thank you! Do you want to start over?", preferredStyle: UIAlertControllerStyle.alert)
+            alert.addAction(UIAlertAction(title: "Yes", style: UIAlertActionStyle.default, handler: {action in
+                self.questionIndex = 0
+                self.questionsLabel.text = self.questions[self.questionIndex]
+                self.nextQbutton.isHidden = false
+            }))
+            
+            self.present(alert, animated: true, completion: nil)
+        }
+        
+        else if questionIndex < questions.count
+        {
+            questionsLabel.text = questions[questionIndex]
+        }
+        
+    }
+    
+    
+    
+    override var prefersStatusBarHidden: Bool {
+        return true
+    }
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        nextQbutton2.isHidden = true
+        nextQbutton.isHidden = false
+        questionsLabel.text = questions[questionIndex]
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
+    
 }
 
